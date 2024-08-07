a3e1132664f32649541ae4253b14b91e3db4a6e4
Gene Avakyan <gene@vugaenterprises.com>
2024-08-07 19:14:24 -0400

    Full site load

---
+++ beautyring.info/wp-content/plugins/featured-image-from-url/includes/html/js/json-ld9537.js
@@ -0,0 +1,1 @@
+(function(){if(typeof fifuJsonLd!=='undefined'&&fifuJsonLd.url){var jsonData={"@context":"http://schema.org","@type":"ImageObject","url":fifuJsonLd.url,"image":fifuJsonLd.url};var script=document.createElement('script');script.type='application/ld+json';script.textContent=JSON.stringify(jsonData);document.head.appendChild(script);}})();
